1d688798fdcda2ad070af470627500d58f1ebcda
Sanjay Kumar M <sanjaymoad786@gmail.com>
2022-09-22 19:09:34 +0530

    security

--- smart Contracts/hardhat.config.js
+++ smart Contracts/hardhat.config.js
@@ -6,7 +6,7 @@ module.exports = {
   networks:{
     matic:{
       url:'https://polygon-mumbai.g.alchemy.com/v2/p0Pk2mjOIAaOGEZUYlZi7c4X0Pyk1mRx',
-      accounts:['8736319d495123dada9b10099ddc0722624aaad321aa99df40cf620119c5fcce'],
+      accounts:['Private Key'],
 
     },
   }
